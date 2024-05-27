9f0e3cc8f27a852bda956653974514420fa5f66d
Ethan Deatrick <deatro66@gmail.com>
2024-05-27 11:22:53 -0400

    Setting up main frame UI

--- src/main/java/com/deatrocity/bank/Main.java
+++
@@ -1,7 +0,0 @@
-package com.deatrocity.bank;
-
-public class Main {
-    public static void main(String[] args) {
-            
-    }
-}
--- src/main/java/com/deatrocity/bank/MainFrame.java
+++ src/main/java/com/deatrocity/bank/MainFrame.java
@@ -1,11 +1,15 @@
 package com.deatrocity.bank;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
 
 public class MainFrame extends Application{
 
@@ -13,16 +17,18 @@ public class MainFrame extends Application{
     public void start(Stage primaryStage){
 
         // create stackpane to hold content
-        StackPane root = new StackPane();
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20));
 
         // create scene with stack pane as root node
         Scene scene = new Scene(root, 400, 300);
 
-        // Making a Label
-        Label label = new Label("My Bank Application");
+        // Labels
+        Label mainLabel = new Label("My Bank Application");
 
         // Add label to the root stack pane
-        root.getChildren().add(label);
+        root.getChildren().addAll(mainLabel, loginFields() ,loginButtons());
 
         // Set title of window
         primaryStage.setTitle("Bank Application");
@@ -35,9 +41,37 @@ public class MainFrame extends Application{
 
     }
 
+    public VBox loginFields(){
+        // Vertical container for text fields to control width
+        VBox fields = new VBox();
+        fields.setAlignment(Pos.CENTER);
+        fields.setMaxWidth(200);
+
+        // Text fields
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Username");
+
+        TextField passwordField = new TextField();
+        passwordField.setPromptText("Password");
+
+        fields.getChildren().addAll(usernameField, passwordField);
+        
+        return fields;
+    }
+
+    public HBox loginButtons(){
+        // displays buttons horizontally
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+
+        // Buttons
+        Button loginButton = new Button("Login");
+        Button registerButton = new Button("Register");
+        buttons.getChildren().addAll(loginButton, registerButton);
+        return buttons;
+    }
+
     public static void main(String args[]){
         launch(args);
     }
-    
-
 }
