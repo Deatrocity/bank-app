eda4a5a76fbd7f6e4be8e24190060f95ac70901d
Ethan Deatrick <edeatrick1@ivytech.edu>
2024-06-13 20:22:55 -0400

    Added sqlite database

--- src/main/java/com/deatrocity/bank/ArrayList.java
+++
@@ -1,5 +0,0 @@
-package com.deatrocity.bank;
-
-public class ArrayList {
-
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.deatocity.bank {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
     exports com.deatrocity.bank;
 }
